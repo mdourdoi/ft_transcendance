be0f92e405419e0096019b0efed3c01613bb32f9
sgil--de <sgil--de@fedora.home>
2026-08-02 12:29:51 +0200

    feat(plateau): Add plateau cell color

--- frontend/src/lib/components/game/plateau/Plateau.svelte
+++ frontend/src/lib/components/game/plateau/Plateau.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
     import Cell from "$lib/components/game/cell/Cell.svelte"
-    let { width = "3rem", height = "3rem", size = 5 } = $props()
+    let { width = "9rem", height = "9rem", size = 5 } = $props()
 </script>
 
 {#each Array(size) as _, i}
     <div class="grid grid-cols-5">
         {#each Array(size) as _, j}
-            <Cell width={width} height={height} color={(i + j) % 2 === 0 ? "white" : "black-800"} />
+            <Cell width={width} height={height} color={(i + j) % 2 === 0 ? "var(--primary)" : "var(--secondary)"} />
         {/each}
     </div>
 {/each}
