be556483ec9a454080f73de8333058feb54fd6be
mdourdoi <m.dourdoigne@hotmail.fr>
2026-08-31 18:32:11 +0200

    init prisma model and dto

---
+++ backend/src/auth/dto/register.dto.ts
@@ -0,0 +1,11 @@
+import { IsEmail, IsString, MinLength } from 'class-validator';
+
+export class RegisterDto {
+  @IsEmail()
+  email: string;
+  @IsString()
+  username: string;
+  @IsString()
+  @MinLength(8)
+  password: string;
+}
