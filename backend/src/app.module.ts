7f78bfa3b65d2a513a07cf96f5beb03d384bc750
AstarophTY <astarophty@gmail.com>
2026-09-08 00:25:55 +0200

    chore: remove boilerplate test endpoint
    Drop the scaffolded AppController/AppService (GET /test) that came with
    the Nest init; nothing depends on them.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 
@@ -13,7 +11,5 @@ import { UsersModule } from './users/users.module';
     AuthModule,
     UsersModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
