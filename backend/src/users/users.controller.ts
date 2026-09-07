014c2b04ce3972f65c6074999db249e120fcb7b6
mdourdoi <m.dourdoigne@hotmail.fr>
2026-09-07 13:58:05 +0200

    feat: Username and mail modification

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Patch, Req, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { JwtGuard } from '../auth/jwt.guard';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -11,5 +12,11 @@ export class UsersController {
 	@Get('me')
 	me(@Req() req) {
 		return this.userService.me(req.user.sub);
-	}
+  }
+
+  @UseGuards(JwtGuard)
+  @Patch('me')
+  update(@Req() req, @Body() dto: UpdateUserDto) {
+    return this.userService.update(req.user.sub, dto)
+  }
 }
