36f0aa5c4ae2cacdbd452ce6f5436a9170f41ffd
mdourdoi <m.dourdoigne@hotmail.fr>
2026-09-10 18:23:50 +0200

    review Swann

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -15,6 +15,7 @@ import { ErrorCode } from '../common/error-codes';
 import { MIME_TO_EXT } from '../common/mime-types';
 import { AVATAR_UPLOAD_DIR } from '../constants';
 import { ChangePasswordDto } from './dto/change-password.dto';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class UsersService {
@@ -58,7 +59,7 @@ export class UsersService {
         avatarUrl: row.avatarUrl,
       };
     } catch (e) {
-      if (e.code === 'P2002')
+      if (e instanceof Prisma.PrismaClientKnownRequestError && e.code === 'P2002')
         throw new ConflictException(ErrorCode.USERNAME_OR_EMAIL_ALREADY_TAKEN);
       throw e;
     }
@@ -104,16 +105,15 @@ export class UsersService {
   }
 
   async changePassword(userId: number, dto: ChangePasswordDto) {
-    if (dto.newPassword === dto.oldPassword) {
-      throw new BadRequestException(ErrorCode.PASSWORD_UNCHANGED);
-    }
-
     const row = await this.prisma.user.findUnique({
       where: { id: userId },
     });
 
     if (!row) {
       throw new NotFoundException(ErrorCode.USER_NOT_FOUND);
+    }
+    if (dto.newPassword === dto.oldPassword) {
+      throw new BadRequestException(ErrorCode.PASSWORD_UNCHANGED);
     }
     const corresponding = await bcrypt.compare(dto.oldPassword, row.passwordHash);
     if (!corresponding) {
